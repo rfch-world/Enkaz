d765fe1ec76e388244142187ee1751ef47063e04
abbaszade <muradabbaszade6@gmail.com>
2023-02-07 04:33:31 +0400

    Delete secondhand directory

--- secondhand/src/main/java/com/secondhand/secondhand/user/repository/AdvertisementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.secondhand.secondhand.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.secondhand.secondhand.user.model.Advertisement;
-
-public interface AdvertisementRepository extends JpaRepository<Advertisement, String> {
-
-}
--- secondhand/src/main/java/com/secondhand/secondhand/user/repository/UserDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.secondhand.secondhand.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.secondhand.secondhand.user.model.UserDetails;
-
-public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer> {
-
-}
